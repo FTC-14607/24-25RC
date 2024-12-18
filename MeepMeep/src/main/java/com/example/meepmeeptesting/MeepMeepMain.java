17309aff9b26792677edb65bc7663acc9e7decb5
R-Tacoz <roccozxyt@gmail.com>
2024-12-17 22:11:39 -0500

    4+0 auto path in meepmeep

--- MeepMeep/src/main/java/com/example/meepmeeptesting/MeepMeepMain.java
+++ MeepMeep/src/main/java/com/example/meepmeeptesting/MeepMeepMain.java
@@ -2,6 +2,7 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.MarkerCallback;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -21,22 +22,44 @@ public class MeepMeepMain {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
-                .addEntity(myBot2)
+//                .addEntity(myBot2)
                 .start();
     }
 
     static RoadRunnerBotEntity getSpecimenPath(MeepMeep meepMeep) {
+        MarkerCallback driveMode = () -> {};
+        MarkerCallback prepareScoreSpecimen = () -> {}; // robot.startPrepareSpecimenDeposit()
+        MarkerCallback releaseScoredSpecimen = () -> {}; // robot.openUpperClaw();
+        MarkerCallback preparePickupSpecimen = () -> {}; // robot.startPrepareSpecimenPickup()
+        MarkerCallback pickupSpecimen = () -> {}; // robot.pickupSpecimen();
+        MarkerCallback prepareAscent = () -> {}; // robot.prepareAscent1();
+
+        Pose2d startPose = new Pose2d(12, -60, Math.toRadians(90));
+
+        Pose2d trussPose =     new Pose2d(10, -32, Math.toRadians(90));
+        Pose2d trussInterval = new Pose2d(3, 0, 0); // dist between scored specimens
+        Pose2d trussBackup =   new Pose2d(0, 2, 0); // dist in front of truss to go straight for
+
+        Pose2d pickupPose =   new Pose2d(38, -61, Math.toRadians(-90));
+        Pose2d pickupBackup = new Pose2d(0, 6, 0); // dist in front of wall to go straight for
+
+        Pose2d parkIntermediate = new Pose2d(-38, -30, Math.toRadians(90));
+        Pose2d parkPose = new Pose2d(-26, -12, 0);
+
         return new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(40, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(12, -60, Math.toRadians(90)))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(startPose)
 
                 // score first pre-loaded specimen
-                .splineToConstantHeading(new Vector2d(10, -34), Math.toRadians(90))
-                .lineTo(new Vector2d(10, -32))
+                .splineToConstantHeading(trussPose.minus(trussBackup).vec(), trussPose.getHeading())
+                .addTemporalMarker(0, prepareScoreSpecimen)
+                .lineTo(trussPose.vec())
+                .addDisplacementMarker(releaseScoredSpecimen)
                 .setReversed(true)
 
                 // move three alliance samples to human player
+                .addTemporalMarker(2, driveMode)
                 .splineToConstantHeading(new Vector2d(36, -28), Math.toRadians(90))
 
                 .splineToConstantHeading(new Vector2d(36, -12), Math.toRadians(90))
@@ -49,33 +72,47 @@ public class MeepMeepMain {
 
                 .splineToConstantHeading(new Vector2d(53, -12), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(61, -12), Math.toRadians(-90))
-                .lineTo(new Vector2d(61, -45))
+                .lineTo(new Vector2d(61, -51))
 
                 // score three specimens
 //                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(61, -55, Math.toRadians(90)))
-                .splineToConstantHeading(new Vector2d(7,-34), Math.toRadians(90))
-                .lineTo(new Vector2d(7, -32))
-                .setReversed(true)
-                .splineToConstantHeading(new Vector2d(38,-55), Math.toRadians(-90))
-                .lineTo(new Vector2d(38, -61))
                 .setReversed(false)
-                .lineTo(new Vector2d(38, -55))
+                .splineToConstantHeading(pickupPose.plus(pickupBackup).vec(), pickupPose.getHeading())
+                        .addTemporalMarker(10.4, preparePickupSpecimen)
+                .lineTo(pickupPose.vec())
+                        .addDisplacementMarker(pickupSpecimen)
+                .lineTo(pickupPose.plus(pickupBackup).vec())
 
-                .splineToConstantHeading(new Vector2d(4,-34), Math.toRadians(90))
-                .lineTo(new Vector2d(4, -32))
+                .splineToConstantHeading(trussPose.minus(trussBackup).minus(trussInterval.times(1)).vec(), trussPose.getHeading())
+                .addTemporalMarker(12.5, prepareScoreSpecimen)
+                .lineTo(trussPose.minus(trussInterval.times(1)).vec())
+                .addDisplacementMarker(releaseScoredSpecimen)
                 .setReversed(true)
-                .splineToConstantHeading(new Vector2d(38,-55), Math.toRadians(-90))
-                .lineTo(new Vector2d(38, -61))
+                .splineToConstantHeading(pickupPose.plus(pickupBackup).vec(), pickupPose.getHeading())
+                .lineTo(pickupPose.vec())
                 .setReversed(false)
-                .lineTo(new Vector2d(38, -55))
+                .lineTo(pickupPose.plus(pickupBackup).vec())
 
-                .splineToConstantHeading(new Vector2d(1,-34), Math.toRadians(90))
-                .lineTo(new Vector2d(1, -32))
+                .splineToConstantHeading(trussPose.minus(trussBackup).minus(trussInterval.times(2)).vec(), trussPose.getHeading())
+                .addTemporalMarker(14.8, prepareScoreSpecimen)
+                .lineTo(trussPose.minus(trussInterval.times(2)).vec())
+                .addDisplacementMarker(releaseScoredSpecimen)
+                .setReversed(true)
+                .splineToConstantHeading(pickupPose.plus(pickupBackup).vec(), pickupPose.getHeading())
+                .lineTo(pickupPose.vec())
+                .setReversed(false)
+                .lineTo(pickupPose.plus(pickupBackup).vec())
+
+                .splineToConstantHeading(trussPose.minus(trussBackup).minus(trussInterval.times(3)).vec(), trussPose.getHeading())
+                .addTemporalMarker(19, prepareScoreSpecimen)
+                .lineTo(trussPose.minus(trussInterval.times(3)).vec())
+                .addDisplacementMarker(releaseScoredSpecimen)
                 .setReversed(true)
 
                 // move to truss for Ascent Level 1
-                .splineToConstantHeading(new Vector2d(-38, -30), Math.toRadians(90))
-                .splineTo(new Vector2d(-26,-12), Math.toRadians(0))
+                .splineToConstantHeading(parkIntermediate.vec(), parkIntermediate.getHeading())
+                .addDisplacementMarker(prepareAscent)
+                .splineTo(parkPose.vec(), parkPose.getHeading())
 
                 .build());
     }
